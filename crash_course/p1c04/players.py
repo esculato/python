e89e7a2d303f9fc5617bd0aa2aed1351498c30d4
esculato <esculato@inbox.lv>
2023-01-18 20:38:52 +0100

    slice backhand!

---
+++ crash_course/p1c04/players.py
@@ -0,0 +1,6 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3:2])
+
+print("Here are the first three players of my team:")
+for player in players[:3]:
+    print(player.title())
--- crash_course/p1c04/squares.py
+++ crash_course/p1c04/squares.py
@@ -2,6 +2,9 @@ squares = []
 for value in range(1, 11):
  #   square = value ** 2
  #   squares.append(square)
-    squares.append(value ** 1)
+    squares.append(value ** 2)
 
+print(squares)
+
+squares = [value**2 for value in range(1,11)]
 print(squares)
