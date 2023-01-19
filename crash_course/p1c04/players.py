c8f52fad3a605e49185d1cfa7cada9962867cbe9
esculato <esculato@inbox.lv>
2023-01-19 10:58:55 +0100

    three to two

--- crash_course/p1c04/players.py
+++ crash_course/p1c04/players.py
@@ -1,6 +1,6 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3:2])
 
-print("Here are the first three players of my team:")
-for player in players[:3]:
+print("Here are the first two players of my team:")
+for player in players[:2]:
     print(player.title())
