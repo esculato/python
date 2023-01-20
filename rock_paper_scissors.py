e0d1d4ce9b2b4cb54c2d5224d93af6831933a7dc
esculato <esculato@inbox.lv>
2023-01-20 15:25:55 +0100

    ifelifelseorwhat

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,24 @@
 import random
 
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors: ")
-    computer_choice = "paper"
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock","paper","scissors"]  
+    computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
-choices = get_choices()
-print(choices)
+#choices = get_choices()
+#print(choices)
 
-## To continue: https://www.youtube.com/watch?v=eWRfhZUzrAc&t=1108s
+def check_win(player, computer):
+  # print("You chose " + player + ". computer chose" + computer)
+    print(f"You chose {player}, computer chose {computer}.") # f-string
+    if player == computer:
+        return "It's a tie!" #parenthesis are option () with return
+    elif player 
+    return [player, computer]
+
+check_win("rock","paper")
+
+
+## To continue: https://youtu.be/eWRfhZUzrAc?t=1901
