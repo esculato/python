dbc1629c1dbe562492fa26f337c04db2b560ee37
esculato <esculato@inbox.lv>
2023-01-31 15:19:57 +0100

    added old excersices

---
+++ general.py
@@ -0,0 +1,16 @@
+name = "Marc" ; print(name)
+age = 42
+
+print(isinstance(name, str))
+
+
+types:
+complex for complex_numbers
+bool for booleans
+list for lists
+tuple for tuples
+range for ranges
+dict for dictionaries
+set for sets
+  
+operators:
