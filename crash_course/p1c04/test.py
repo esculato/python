7ac2dd22c576f4637c0549ee5d3f523d23c39fa3
esculato <esculato@inbox.lv>
2023-06-05 17:15:06 +0200

    test git (CAN BE REMOVED)

---
+++ crash_course/p1c04/test.py
@@ -0,0 +1,10 @@
+squares = []
+for value in range(1, 11):
+ #   square = value ** 2
+ #   squares.append(square)
+    squares.append(value ** 2)
+
+print(squares)
+
+squares = [value**2 for value in range(1,11)]
+print(squares)
