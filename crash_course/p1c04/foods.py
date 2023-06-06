42014312714e4b321bf47325b9eb70ca3adcfa54
esculato <esculato@inbox.lv>
2023-06-06 16:55:37 +0200

    picking up the sloooooog

---
+++ crash_course/p1c04/foods.py
@@ -0,0 +1,17 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+#print(my_foods)
+
+#print("\nMy friend's favorite foods are:")
+#print(friend_foods)
+
+my_foods.append('cannoli')
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+friend_foods.append('ice cream')
+print(friend_foods)
+
+
--- crash_course/p1c04/magicians.py
+++ crash_course/p1c04/magicians.py
@@ -1,6 +1,12 @@
 magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
-    print(f'{magician.title()}, that was a great trick!')
-    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+#for magician in magicians:
+#    print(f'{magician.title()}, that was a great trick!')
+#    print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
-print("Thank you, everyone. That was a great magic show!")
+#print("Thank you, everyone. That was a great magic show!")
+
+print(magicians[2:])
+
+for magician in magicians[0:3]:
+    print(f'{magician.title()}')
+    print(f'{magician.title()}')
