d98fe69568595e933ac833064fc9f4b75705a3c3
esculato <esculato@inbox.lv>
2023-06-06 20:18:53 +0200

    tuples of tulips? ;)

---
+++ crash_course/p1c04/tuples.py
@@ -0,0 +1,16 @@
+dimensions = (200,50)
+#dimensions[0] = 250
+#print(dimensions[0])
+#print(dimensions[1])
+
+#for dimension in dimensions:
+#    print(dimension)
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100) 
+print("\nModified dimensions:") 
+for dimension in dimensions:
+    print(dimension)
