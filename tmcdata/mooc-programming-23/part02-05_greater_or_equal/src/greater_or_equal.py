bba9edccb68b74604d63e0312ee7ca387669d3bd
esculato <esculato@inbox.lv>
2023-06-15 12:30:58 +0200

    exercise test in git

--- tmcdata/mooc-programming-23/part02-05_greater_or_equal/src/greater_or_equal.py
+++ tmcdata/mooc-programming-23/part02-05_greater_or_equal/src/greater_or_equal.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+number1 = int(input("Please type in the first number: "))
+number2 = int(input("Please type in another number: "))
+
+if number1 > number2:
+    print(f"The greater number was: {number1} ")
+elif number2 > number1:
+    print(f"The greater number was: {number2} ")
+else:
+    print("The numbers are equal!")
+
