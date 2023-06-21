91a36b83f3fff2c23423784e78a97ad1e3ffadff
esculato <esculato@inbox.lv>
2023-06-21 20:00:09 +0200

    we have a greater_or_equal

---
+++ tmcdata/mooc-programming-23/part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
@@ -0,0 +1,15 @@
+# Write your solution here
+
+while True:
+    pin = int(input("PIN: "))
+    attempts += 1
+
+    if code == "4321":
+        success = True
+        break
+
+if success = True and attempts == 1:
+    print("Correct! It only took you one single attempt!")
+elif success = True and attempts > 1:
+    print(f"Correct! It took you {attempts} attempts")
+
