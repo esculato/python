3bc188033fc1af9f31ebe12aa8706353ae62003b
esculato <esculato@inbox.lv>
2023-06-22 09:56:08 +0200

    part 2... done!

--- tmcdata/mooc-programming-23/part02-21_story/src/story.py
+++ tmcdata/mooc-programming-23/part02-21_story/src/story.py
@@ -2,13 +2,19 @@
 
 word = "" 
 words = ""
+attempt = 0
 
 while True:
     prevword = word
     word = input("Please type in a word: ")
-    if word == prevword:
+    attempt += 1
+    if word == "end":
         break
+    elif word == prevword:
+        if attempt > 1:
+            break
     else:
         words += word + " "
+
 
 print(f"{words}")
--- tmcdata/mooc-programming-23/part02-22_working_with_numbers/src/working_with_numbers.py
+++ tmcdata/mooc-programming-23/part02-22_working_with_numbers/src/working_with_numbers.py
@@ -1,3 +1,27 @@
 # Write your solution here
 
-test
+print("Please type in integer numbers. Type in 0 to finish.")
+
+count = 0
+sum = 0
+positive = 0
+negative = 0
+
+while True:
+    number = int(input("Number: "))
+    if number == 0:
+        break
+    count += 1
+    sum += number
+    mean = sum / count
+    if number > 0:
+        positive += 1
+    elif number < 0:
+        negative += 1
+
+print("... the program asks for numbers")
+print(f"Numbers typed in {count}")
+print(f"The sum of the numbers is {sum}")
+print(f"The mean of the numbers is {mean}")
+print(f"Positive numbers {positive}")
+print(f"Negative numbers {negative}")
