54f3dc183261369d73ea1ae98970aa67a784608f
esculato <esculato@inbox.lv>
2023-06-28 17:35:13 +0200

    strings + strings = more strings

--- tmcdata/mooc-programming-23/part03-09_longer_string/src/longer_string.py
+++ tmcdata/mooc-programming-23/part03-09_longer_string/src/longer_string.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+string1 = input("Please type in string 1: ")
+string2 = input("Please type in string 2: ")
+
+if len(string1) > len(string2):
+    print(f"{string1} is longer")
+elif len(string1) < len(string2):
+    print(f"{string2} is longer")
+else:
+    print("The strings are equally long")
