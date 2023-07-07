095ca3a8c67691c490e76bda00998ad737575d75
esculato <esculato@inbox.lv>
2023-07-07 12:31:05 +0200

    complicated

--- tmcdata/mooc-programming-23/part03-15_right_aligned/src/right_aligned.py
+++ tmcdata/mooc-programming-23/part03-15_right_aligned/src/right_aligned.py
@@ -1,1 +1,18 @@
 # Write your solution here
+
+string = input("Please type in a string: ")
+line = 20 - len(string)
+star = ""
+x = 1
+
+while x <= line:
+    star += "*"
+    x += 1
+
+print(star + string)
+
+""" word = input("Please type in a string: ")
+ 
+aligned = (20 - len(word)) * "*" + word
+ 
+print(aligned) """
--- tmcdata/mooc-programming-23/part03-16_framed_word/src/framed_word.py
+++ tmcdata/mooc-programming-23/part03-16_framed_word/src/framed_word.py
@@ -1,1 +1,37 @@
 # Write your solution here
+
+word = input("Word: ")
+x = 1
+frame = ""
+
+if len(word) % 2 ==  0:
+    space1 = ((28 - len(word)) // 2) * " "
+else:
+    space1 = (((28 - len(word)) // 2) + 1) * " " 
+
+space2 = ((28 - len(word)) // 2) * " "
+
+middle = "*" + space1 + word + space2 + "*"
+
+while x <= 30:
+    frame += "*"
+    x += 1
+
+print(frame)
+print(middle)
+print(frame)
+
+""" word = input("Word: ")
+ 
+print("*" * 30)
+spaces_at_start = (28 - len(word)) // 2
+spaces_at_end = spaces_at_start
+ 
+# If the word length is odd, one is added to the spaces at the end of the word
+# to get all 30 characters filled
+if len(word) % 2 != 0:
+    spaces_at_end += 1
+ 
+print("*" + spaces_at_start * " " + word + spaces_at_end * " " + "*")
+print("*" * 30) """
+ 
